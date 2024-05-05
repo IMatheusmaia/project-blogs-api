1942ed65827a1d2da3d4d32b458d20ca722df0fb
IMatheusmaia <i.matheuslmaia@gmail.com>
2024-05-05 17:59:15 -0300

    criando model para User

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+const userModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User',
+  {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  },
+{
+  tableName: 'users',
+  timeStamps: false,
+  underscored: true,
+});
+  return User;
+};
+
+module.exports = userModel;
